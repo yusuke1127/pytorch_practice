d358169a2291ce81a55f2c87a109ee4cd7594501
yusuke1127 <ysk.u.kme32433.k@gmail.com>
2024-04-25 05:19:19 +0900

    t3 all completed

--- pytorch_pr3.py
+++ pytorch_pr3.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import Dataset, DataLoader, random_split
+from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
@@ -58,66 +58,61 @@ class RNNModel(nn.Module):
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.rnncell = nn.RNNCell(input_size, hidden_size)
-        self.fc = nn.Linear(hidden_size, output_size)
          
     def forward(self, x, hidden):
         hidden = self.rnncell(x, hidden)
-        output = hidden
+        output = torch.atanh(hidden)
         output = output.view(-1, 2)
         return output, hidden
-"""
-class RNN_Net(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super().__init__()
-        self.rnn = RNNModel(input_size, hidden_size)
-        self.fc = nn.Linear(hidden_size, output_size)
-    
-    def forward(self, x, hidden):
-        output, h = self.rnn(x, hidden)
-        return output, h
-"""
 
 def train(epochs, model, criterion, optimizer):
-    losses = []
-
     for epoch in range(epochs):
-        running_outputdata = []
-        print('epoch:', epoch)
+        print('epoch: {}'.format(epoch + 1))
         optimizer.zero_grad()
         hidden = torch.zeros(hidden_size).to(device)
         running_loss = 0.
         for x_train, y_train in train_loader:
             for timestep in range(x_train.shape[1]):
-                traindata = x_train[0, 0].to(device)
+                traindata = x_train[0, timestep].to(device)
                 targetdata = y_train[0, timestep].to(device)
                 targetdata = targetdata.view(-1, 2)
-                if timestep == 0:
-                    output, hidden = model(traindata, hidden)
-                else:
-                    output, hidden = model(output[0], hidden)
-                output = torch.atanh(output).to(device)
+                
+                output, hidden = model(traindata, hidden)
                 output, hidden = output.to(device), hidden.to(device)
                 loss = criterion(output, targetdata)
                 running_loss += loss
-                
-                running_outputdata_cell = output.to("cpu").detach().numpy()
-                running_outputdata.append(running_outputdata_cell)
-                    
+            
         running_loss.backward()
         optimizer.step()
-
-        print(f'loss: {running_loss.item() / x_train.shape[1]:.6f}')
-        losses.append(running_loss.item() / x_train.shape[1])
+                
         
+        print('loss: {:.8f}'.format(running_loss.item() / x_train.shape[1]))
         
-        if running_loss.item() / x_train.shape[1] < 0.003:
-            for i in range(x_train.shape[1]):
-                outputdata.append(running_outputdata[i])
+        if running_loss.item() / x_train.shape[1] < 1e-6:
             break
-               
-    return output, losses
+            
+    return output
         
-   
+
+def test(model):
+    hidden = torch.zeros(hidden_size).to(device)
+    
+    with torch.no_grad():
+        for x_test, y_test in train_loader:
+            for timestep in range(x_test.shape[1]):
+                if timestep == 0:
+                    input_co = x_test[0, 0].to(device)
+                else:
+                    input_co = predicted_co.to(device)
+                
+                predicted_co, hidden = model(input_co, hidden)
+                predicted_co = predicted_co[0]
+                predicted_co, hidden = predicted_co.to(device), hidden.to(device)
+                outputdata_cell = predicted_co.to("cpu").detach().numpy()
+                outputdata.append(outputdata_cell)
+             
+    
+
 # Define hyperparameters
 input_size = 2  # Input size
 hidden_size = 2  # Hidden layer size
@@ -131,10 +126,13 @@ model = model.to(device)
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-output, losses = train(num_epochs, model, criterion, optimizer)
+
+
+output = train(num_epochs, model, criterion, optimizer)
+
+test(model)
 
 outputdata = np.array([arr for arr in outputdata])
-outputdata = np.array([inner[0] for inner in outputdata])
 
  
 # Output the results
